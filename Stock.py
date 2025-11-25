e5ee65934cc7484ee7f2263824c3d27afc27c29a
RamDk1 <ramses2002rb@gmail.com>
2025-11-25 16:09:23 -0700

    updated

--- Stock.py
+++ Stock.py
@@ -1,8 +1,4 @@
-# Python code​​​​​​‌‌‌​‌​‌​‌​‌‌​‌​‌​‌​‌‌​​​‌ below
-# Use print("messages...") to debug your solution.
 
-show_expected_result = False
-show_hints = False
 
 class Stock:
     def __init__(self, ticker, price, company):
@@ -13,15 +9,9 @@ class Stock:
     def get_description(self):
         return print(f"{self.ticker}: {self.company} -- ${self.price}")
     
-
-
-# This is how your code will be called.
-# DO NOT change these variable names. You CAN change the values.
 ticker = "GOOG"
 price = 185.43
 company = "Google LLC"
 
-# DO NOT change this code:
 symbol = Stock(ticker, price, company)
-
 description = symbol.get_description()
